82d779f08e0bd8fbf266fc62d6a91edc12819068
OzInFl <53567373+OzInFl@users.noreply.github.com>
2022-08-10 23:46:32 -0400

    Update ui_helpers.c

--- src/ui_helpers.c
+++ src/ui_helpers.c
@@ -1,7 +1,7 @@
 // SquareLine LVGL GENERATED FILE
 // EDITOR VERSION: SquareLine Studio 1.1.0 Beta1
 // LVGL VERSION: 8.2
-// PROJECT: M5StackCore2_GHOSTOOL
+// PROJECT: M5StackCore2_LVGL8_Test
 
 #include "ui_helpers.h"
 
